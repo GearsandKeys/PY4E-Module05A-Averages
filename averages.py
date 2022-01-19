d0ee1e96cbb06a95349be83568d384defe957853
Nathan Gearke <Nathan.Gearke@vu.com>
2022-01-19 15:48:55 -0600

    Adjusted code for autograder

--- averages.py
+++ averages.py
@@ -1,6 +1,5 @@
 def averages():
     #Write code below this line, remember that lines in a functions are indented!
-    
 
 
 
@@ -8,4 +7,5 @@ def averages():
     #Note: the end should look something like this:
     #print(total, count, average)
 
-averages() #Do NOT comment this out or your test will fail since this will 'call' your function.
+averages() #Do NOT comment this out or your test will fail since this will 'call' your function. (This is because it is being tested differently)
+## if you want to test locally before you try to sync run > python averages.py
