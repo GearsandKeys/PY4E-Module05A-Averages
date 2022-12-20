a4483ca899e025ead9fa0eb41a9338bc24eb0c19
Nathan Gearke <69879313+GearsandKeys@users.noreply.github.com>
2022-12-20 13:29:21 -0600

    Update averages.py

--- averages.py
+++ averages.py
@@ -1,11 +1,10 @@
 def averages():
-    #Write code below this line, remember that lines in a functions are indented!
-
-
-
-    
-    #Note: the end should look something like this:
+    # Write code below this line, remember that lines in a functions are indented!
+    # Note: the end should look something like this:
     #print(total, count, average)
 
-averages() #Do NOT comment this out or your test will fail since this will 'call' your function. (This is because it is being tested differently)
 ## if you want to test locally before you try to sync run > python averages.py
+
+
+if __name__ == "__main__":
+    averages() # Here is where your averages function is being called when we run python averages.py!
